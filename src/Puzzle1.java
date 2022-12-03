9b55ccdcc884a9df2ee323612b1ebc33fb7c69f6
Yannis Mouseck <yannis-mouseck@t-online.de>
2022-12-03 02:37:28 +0100

    final version

--- src/Puzzle1.java
+++ src/Puzzle1.java
@@ -5,17 +5,24 @@ import java.util.Scanner;
 public class Puzzle1 {
     public static void main(String[] args) throws FileNotFoundException {
         Scanner in = new Scanner(new File("Puzzle1.txt"));
-        StringBuilder sb = new StringBuilder();
 
-        while(in.hasNext()) {
-            sb.append(in.next());
-            sb.append(" ");
-
-
+        int bestSum = 0;
+        int currentSum = 0;
+        while(in.hasNext()){
+            String s = in.nextLine();
+            if(s.equals("")){
+                if(currentSum > bestSum){
+                    bestSum = currentSum;
+                }
+                currentSum = 0;
+            } else {
+                int x = Integer.parseInt(s);
+                currentSum += x;
+            }
         }
         in.close();
-        String realString = sb.toString();
 
-        System.out.println(realString);
+
+        System.out.println(bestSum);
     }
 }
